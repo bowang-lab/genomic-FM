82914e97911704c60ac1718885a4cd46c238580d
Vallijah Subasri <vallijahsubasri@MacBook-Pro.local>
2024-04-02 13:08:26 -0400

    Add tests for maves and regulatory regions

--- src/datasets/maves/get_maves.py
+++ src/datasets/maves/get_maves.py
@@ -98,17 +98,3 @@ def extract_variant_details(scores):
         variant_details.append(variant_info)
     
     return variant_details
-
-
-# Example usage
-urn_ids = get_all_urn_ids()
-for urn_id in urn_ids[:10]:
-    print(urn_id)
-    score_set = get_score_set(urn_id)
-    print(score_set)
-    for exp in score_set:
-        urn_id = exp.get('urn', None)
-        scores = get_scores(urn_id) 
-        print(scores)
-
-
